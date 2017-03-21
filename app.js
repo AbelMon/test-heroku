5dd670b532fd50079d4ab74f1230a2511c16c3d4
AbelMon <marvinabelmon@gmail.com>
2017-03-21 12:38:01 -0600

    changes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
-var port = process.env.port || 3000;
+var port = process.env.port || 8080;
+
+app.use(__dirname);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(port, function () {
+  console.log('Holis');
   console.log('Example app listening on port ' + String(port));
 });
